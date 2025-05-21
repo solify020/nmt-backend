c064fc57962f80d27cdbbcc55393c7932fceaf5d
solify020 <sim68corwinqmr@hotmail.com>
2025-05-22 00:47:33 +0900

    fix problems and complete

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,15 @@ import express, { Request, Response } from 'express';
 import morgan from "morgan"
 import axios from 'axios';
 import {OpenAI} from 'openai';
-
+import bodyParser from 'body-parser';
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 const port = 5000;
 
-const OPENAI_API_KEY = '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';
+const OPENAI_API_KEY = '';
 const openai = new OpenAI({
   apiKey: OPENAI_API_KEY
 })
@@ -16,9 +19,20 @@ app.use(morgan('dev'))
 
 app.post('/analysis', async (req: Request, res: Response) => {
   try {
+    const token_info = req.body;
     const prompt = `
-    This is my token information that I want to launch. ${req.body}
+    This is my token information that I want to launch.
+    - TokenType: ${token_info.tokenType}
+    - TokenName: ${token_info.tokenName}
+    - TokenDescription: ${token_info.tokenDescription}
+    - dex: ${token_info.dex}
+    - chain: ${token_info.chain}
+    - X_Followers: ${token_info.x_followers}
+    - Telegram_Members: ${token_info.telegram_members}
     Please output only just json format with following fields.
+    You mustn't output any other text except json.
+    You must output start with "{" and end with "}".
+    Don't output like "Please provide...", "of course" etc.
     {
         similarTokens: [<Token>],
         analyze: <Analyze>
@@ -40,10 +54,16 @@ app.post('/analysis', async (req: Request, res: Response) => {
       way_to_success: string,
     }
 
+    My token is not launched yet.
+    It is what I want to launch.
+    search similar name or desscription tokens online and same blockchain.
+    similar tokens chain must be same as my token's chain.
+    Each data of similar tokens mustn't be null, must have value (must fetch data from online web search).
+    If my chain is Solana, the similar tokens' chain must be Solana.
     analyze_result is the result of analyzing the token.
     success_rate is the success rate of the token.
     way_to_success is the way to success of the token.
-  `
+    `
   
   // const response = await openai.chat.completions.create({
   //   model: "gpt-4",
@@ -53,13 +73,16 @@ app.post('/analysis', async (req: Request, res: Response) => {
   //   ]
   // });
 
-    const response = await openai.responses.create({
+    const response = (await openai.responses.create({
       model: "gpt-4.1",
       tools: [{type: "web_search_preview_2025_03_11"}],
       input: prompt
-    })
+    })).output_text;
 
-    res.json(response);
+    const result = JSON.parse(response.replace("```json", "").replace("```", "").replace("\n", ""));
+
+
+    res.json(result);
   } catch (err: any) {
     console.error("agafasdfasdasfas", err);
     res.status(500).json({ error: 'Internal server error' });
